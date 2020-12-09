d35d1c33a44e7117a1afec7abedc6db57eba72fb
Anand <anandsnair2005@gmail.com>
2020-12-09 23:03:12 +0530

    Added Order functionality

---
+++ nonrest/src/main/java/payroll/Order.java
@@ -0,0 +1,26 @@
+package payroll;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER_ORDER") @NoArgsConstructor @ToString @EqualsAndHashCode
+public class Order {
+    @Getter @Setter
+    private @Id @GeneratedValue Long id;
+
+    @Getter @Setter
+    private String description;
+
+    @Getter @Setter
+    private Status status;
+
+    public Order(String description, Status status) {
+        this.description = description;
+        this.status = status;
+    }
+}
